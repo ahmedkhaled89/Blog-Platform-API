91f783b6f9d6252344c8a85cd8f33135c7a029a3
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-09-07 00:36:19 +0300

    use Post route in our express app

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 import express from 'express';
+import { postsRoutes } from './routes/PostsRoutes.js';
 import mongoose from 'mongoose';
 
 const app = express();
 
 app.use(express.json());
 
+app.use('/api/posts', postsRoutes);
+
 mongoose
-  .connect('mongodb://localhost:27017')
+  .connect('mongodb://localhost:27017', { dbName: 'demo_db' })
   .then(() => {
     console.log('mongoDB connected successfully');
     app.listen(4000, () => {

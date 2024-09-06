53a9decfaac174b3fc4eb922c4f16b1a7ed5ad1e
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-09-07 00:32:24 +0300

    Add Post model

---
+++ models/PostsModel.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema(
+  {
+    title: { type: String, required: true },
+    body: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model('Post', PostSchema);
+
+export default Post;

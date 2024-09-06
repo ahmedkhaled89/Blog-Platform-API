efc5f3dabf59ac6603da88668a9335dd6f42923a
ahmedkhaled <ahmedkhaled8901@gmail.com>
2024-09-07 00:56:09 +0300

    refactor postsRoutes functions to a controller file

---
+++ controllers/postsController.js
@@ -0,0 +1,32 @@
+import Post from '../models/PostsModel.js';
+
+// ******************* Get All Posts *******************
+export const getAllPosts = async (req, res) => {
+  try {
+    const posts = await Post.find();
+    if (!posts) {
+      return res.status(404).json({ status: 'FAIL', msg: 'No Posts' });
+    }
+    res.status(200).json({ status: 'SUCCESS', data: { posts } });
+  } catch (error) {
+    return res.status(500).json({ status: 'FAIL', error: error.message });
+  }
+};
+// ******************* Create New Post *******************
+export const createPost = async (req, res) => {
+  // Grab Data
+  const { title, body } = req.body;
+
+  // Validate Date
+  if (!title.trim() || !body.trim()) {
+    return res
+      .status(400)
+      .json({ status: 'FAIL', error: 'All Fields are Required' });
+  }
+  try {
+    const post = await Post.create({ title, body });
+    res.status(201).json({ status: 'SUCCESSES', data: { post } });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
